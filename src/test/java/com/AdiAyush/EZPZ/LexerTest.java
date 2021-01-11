9f2a3c75c63b73222ccdc26849396b98b9e0017f
adiswami14 <adithya1431@gmail.com>
2021-01-10 21:11:23 -0500

    Change test file extensions

--- src/test/java/com/AdiAyush/EZPZ/LexerTest.java
+++ src/test/java/com/AdiAyush/EZPZ/LexerTest.java
@@ -19,14 +19,14 @@ import org.junit.Test;
 public class LexerTest{
     @Test
     public void BasicStateTest() throws FileNotFoundException {
-        Lexer testLexer = new Lexer(new File("/Users/ayush/ezpz/src/test/java/com/AdiAyush/EZPZ/testCode/test.txt"));
+        Lexer testLexer = new Lexer(new File("src/test/java/com/AdiAyush/EZPZ/testCode/test.txt"));
         String testInput = testLexer.parseInput();
         assertEquals("x = 1 + 3;\ny = x + 5;\n", testInput);
     }
 
     @Test
     public void ReadTokenTest() throws FileNotFoundException {
-        Lexer testLexer = new Lexer(new File("/Users/ayush/ezpz/src/test/java/com/AdiAyush/EZPZ/testCode/test.txt"));
+        Lexer testLexer = new Lexer(new File("src/test/java/com/AdiAyush/EZPZ/testCode/test.txt"));
         testLexer.readToken();
         boolean equalZero = testLexer.currentPosition == 0;
         assertEquals(true, equalZero);
@@ -48,7 +48,7 @@ public class LexerTest{
 
     @Test
     public void LexerTokenTest() throws FileNotFoundException {
-        Lexer testLexer = new Lexer(new File("/Users/ayush/ezpz/src/test/java/com/AdiAyush/EZPZ/testCode/test.txt"));
+        Lexer testLexer = new Lexer(new File("src/test/java/com/AdiAyush/EZPZ/testCode/test.txt"));
         System.out.println("hello");
         ArrayList<Token> list = testLexer.tokenize();
         System.out.println(list);
